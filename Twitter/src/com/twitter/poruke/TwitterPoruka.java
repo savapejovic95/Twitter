9307180b7cce8d4998f069e68e8d4e1c576c05f1
savapejovic95 <sava.pejovic@yahoo.com>
2016-03-23 18:05:54 +0100

    Napisana kompletna JavaDoc dokumentacija za klasu TwitterPoruka, njene atribute i metode

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,14 +1,39 @@
 package com.twitter.poruke;
 
+/**
+ * Klasa koja predstavlja twitter poruku
+ * @author Sava Pejovic
+ * @version 1.0
+ *
+ */
 public class TwitterPoruka {
 
+	/**
+	 * Ime korisnika
+	 */
 	private String korisnik;
+	/**
+	 * Sadrzaj poruke
+	 */
 	private String poruka;
 	
+	/**
+	 * Vraca ime korisnika
+	 * @return ime korisnika
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 	
+	/**
+	 * Postavlja novu vrednost za ime korisnika
+	 * @param korisnik Nova vrednost za ime korisnika
+	 * @throws java.lang.RuntimeException ako je uneto ime
+ 	 * <ul> 
+ 	 * 		<li>null</li>
+ 	 * 		<li>prazan String</li>
+ 	 * </ul>
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik==null || korisnik == "")
 			throw new RuntimeException(
@@ -17,10 +42,20 @@ public class TwitterPoruka {
 		this.korisnik = korisnik;
 	}
 	
+	/**
+	 * Vraca sadrzaj poruke
+	 * @return sadrzaj poruke
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
 	
+	/**
+	 * Postavlja novi sadrzaj poruke
+	 * @param poruka Novi sadrzaj poruke
+	 * @throws java.lang.RuntimeException ako sadrzaj poruke ima null vrednost 
+	 * ili ako ima vise od 140 znakova
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka==null || this.poruka.length()>140)
 			throw new RuntimeException(
@@ -29,6 +64,9 @@ public class TwitterPoruka {
 		this.poruka = poruka;
 	}
 	
+	/**
+	 * @return ime korisnika & sadrzaj poruke
+	 */
 	public String toString(){
 		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
 	} 
